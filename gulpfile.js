f4bab73f624b0acf846ed5e70834dee819b888ce
肖孟智 <xmz@xiaomengzhideMacBook-Pro.local>
2019-06-26 01:48:22 +0800

    解决gulp批量监听.scss 文件生成.css 的问题

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const path = require('path');
 const fs = require('fs');
-const merge2 = require('merge2');
+const merge = require('merge-stream');
+const watch = require('gulp-watch');
 // 获取路径
-
-const watchPath = './components/**/style/*.scss';
-// 不用填写后缀
-const outPut = './components/button/style';
 
 function getFolders(dir) {
   return fs.readdirSync(dir).filter(function(file) {
@@ -19,20 +16,16 @@ const scssPath = 'components';
 
 //任务1:sass->css:
 gulp.task('compileSass', function() {
-  const folders = getFolders(scssPath);
+  const folders = getFolders(scssPath); // button style 等
   const tasks = folders.map(function(folder) {
-    var inPath = `./${scssPath}/${folder}/style/*.scss`;
-    var outPath = `./${scssPath}/${folder}/style`;
-
-    return gulp
-      .src(inPath)
-      .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-      .pipe(outPath);
+    return watch(path.join(scssPath, folder, 'style/*.scss'), () => {
+      gulp
+        .src(path.join(scssPath, folder, 'style/*.scss'))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(gulp.dest(path.join(scssPath, folder, 'style')));
+    });
   });
-  console.log(tasks);
-  return merge2(tasks);
+  return merge(tasks);
 });
 
-gulp.task('default', function() {
-  gulp.watch(scssPath, gulp.series('compileSass'));
-});
+gulp.task('default', gulp.series('compileSass'));
